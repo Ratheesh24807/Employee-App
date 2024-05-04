3f15c85b5275fe7aa6719917fe65ffc8aa7f465d
Ratheesh <ratheeshagowda@gmail.com>
2024-05-04 16:12:48 +0530

    Swag config removed

--- src/main/java/in/ratheesh/entity/Employee.java
+++ src/main/java/in/ratheesh/entity/Employee.java
@@ -11,58 +11,72 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "empdata")
 public class Employee {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	
-	private Integer id;
-	private String name;
-	private String gender;
-	private Integer contactno;
-	//private Integer phno;
-	
-	public Employee() {
-		
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public Integer getContactno() {
-		return contactno;
-	}
-	public void setContactno(Integer contactno) {
-		this.contactno = contactno;
-	}
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", contactno=" + contactno + "]";
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
-	public Employee(Integer id, String name, String gender, Integer contactno) {
-		
-		this.id = id;
-		this.name = name;
-		this.gender = gender;
-		this.contactno = contactno;
-	}
+    private Integer id;
+    private String name;
+    private String gender;
+    private Integer contactno;
+    //private Integer phno;
 
-	
-	
+    public Employee() {
+
+    }
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getGender() {
+
+        return gender;
+    }
+
+    public void setGender(String gender) {
+
+        this.gender = gender;
+    }
+
+    public Integer getContactno() {
+
+        return contactno;
+    }
+
+    public void setContactno(Integer contactno) {
+
+        this.contactno = contactno;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", contactno=" + contactno + "]";
+    }
+
+    public Employee(Integer id, String name, String gender, Integer contactno) {
+
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.contactno = contactno;
+    }
+
+
 }
